d2b8af59c517ba2b5e61a6f33933b64eaa9d62be
wawhal <rishichandra.wawhal@gmail.com>
2017-12-14 18:59:15 +0530

    DeploymentPush

--- microservices/bot/app/src/server.py
+++ microservices/bot/app/src/server.py
@@ -36,13 +36,13 @@ def homepage():
 @ask.launch
 def startSkill():
     quote = getYodaQuote()
-    response = quote + 'hmmm ......... Do you want more?'
+    response = quote + '...... mmmm ......... Do you want more?'
     return question(response)
 
 @ask.intent("YesIntent")
 def shareQuote():
     quote = getYodaQuote()
-    response = quote + 'hmmm ......... Do you want more?'
+    response = quote + '...... mmmm ......... Do you want more?'
     return question(response)
 
 @ask.intent("NoIntent")
