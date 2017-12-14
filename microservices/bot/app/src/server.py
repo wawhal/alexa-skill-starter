dc5afaecd9450be5f285a0a2a4aa5e5c3eabec83
wawhal <rishichandra.wawhal@gmail.com>
2017-12-14 13:36:49 +0530

    Deployment Push

--- microservices/bot/app/src/server.py
+++ microservices/bot/app/src/server.py
@@ -8,38 +8,30 @@ import unidecode
 
 ask = Ask(app, "/reddit_reader")
 
-def get_headlines():
-    user_pass_dict = {'user': '',
-                      'passwd': '',
-                      'api_type': 'json'}
-    sess = requests.Session()
-    sess.headers.update({'User-Agent': 'I am testing Alexa: Sentdex'})
-    sess.post('https://www.reddit.com/api/login', data = user_pass_dict)
-    time.sleep(1)
-    url = 'https://reddit.com/r/worldnews/.json?limit=10'
-    html = sess.get(url)
-    data = json.loads(html.content.decode('utf-8'))
-    titles = [unidecode.unidecode(listing['data']['title']) for listing in data['data']['children']]
-    titles = '... '.join([i for i in titles])
-    return titles  
+def getYodaQuote():
+	url = http://data.hasura/v1/query
+	headers = {
+		"Content-type": "application/json"
+	}
+	body = {}
 
 @app.route('/')
 def homepage():
     return "Alexa skill is running."
 
 @ask.launch
-def start_skill():
+def startSkill():
     welcome_message = 'Hello there, would you like the news?'
     return question(welcome_message)
 
 @ask.intent("YesIntent")
-def share_headlines():
-    headlines = get_headlines()
-    headline_msg = 'The current world news headlines are... {}'.format(headlines)
+def shareQuote():
+    quote = getYodaQuote()
+    headline_msg = 'The current world news headlines are... '+quote
     return statement(headline_msg)
 
 @ask.intent("NoIntent")
-def no_intent():
+def noIntent():
     bye_text = 'I am not sure why you asked me to run then, but okay... bye'
     return statement(bye_text)
 
